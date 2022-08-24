ddf45c1c65a646a2655477688e54346b70f23c9f
Command8 <anrosario@signifyhealth.com>
2022-08-24 14:31:59 -0400

    test_Claims_liquid_temp.py update spacing

--- test_Claims_liquid_temp.py
+++ test_Claims_liquid_temp.py
@@ -2,39 +2,26 @@ import unittest
 import json
 
 trans_claims_resource = '  "resourceType" : "Claim","identifier" : {"use": "official","value": "{{msg.ClaimNumber}}",},'
-
-trans_claims_status = '"status" : "active",'
-
+trans_claims_status = '"status" : "active",' #required
 trans_claims_type = '"type": {"coding": [{"system": "http://terminology.hl7.org/CodeSystem/claim-type","code": "institutional"//Should this be institutional or professional}]},'
-
-trans_claims_use = '"use" : "claim", '
-
+#trans_claims_subtype = More granular claim type
+trans_claims_use = '"use" : "claim", ' #required
 trans_claims_patient =   '"patient" : {"reference": "Patient/{{msg.CenseoID}}"'
-
 trans_claims_billablePeriod = '"billablePeriod": {"start": "{{msg.ServiceFromDate_Date_of_Service | date: "yyyy-MM-dd"}}","end": "{{msg.ServiceThruDate | date: "yyyy-MM-dd"}}"},'
-
 trans_claims_created = '"created" : "{{msg.ClaimEntryDate | date: "yyyy-MM-dd"}}", '
-
 trans_claims_enterer = '"enterer" : {"reference": "Practitioner/{{msg.Provider_ID}}"'
-
+#trans_claims_insurer = Target
 trans_claims_provider = '"provider" : {"reference": "Practitioner/{{msg.Provider_ID}}"},'
-
 trans_claims_priority = '"priority" : { "coding": [{"code": "normal"]},'
-
 trans_claims_prescription = '"prescription" : { "reference": "MedicationRequest/{{msg.MedicationID}}"},"payee" : { "type": {"coding":[{"code": "provider","display": "{{msg.RenderingProviderNPI}}""}]}},'
-
+#trans_claims_originalPrescription = 
 trans_claims_payee = '"payee" : { "type": {"coding":[{"code": "provider","display": "{{msg.RenderingProviderNPI}}""}]}},'
-
+#trans_claims_referral = Treatment referral
 trans_claims_facility = '"facility" : {"identifier":{"value": "{{msg.PlaceOfService}}"}'
-
 trans_claims_careTeam = '"careTeam": [{"sequence": 1,"provider": {"reference": "Practitioner/{{msg.Provider_ID}}"}}],'
-
 trans_claims_diagnosis = '"diagnosis" : [{ "sequence" : 1, "diagnosisCodeableConcept" : { "coding": [{"code": "{{msg.DX_1}}"}]}},{ "sequence" : 2, "diagnosisCodeableConcept" : { "coding": [{"code": "{{msg.DX_2}}"}]}},{ "sequence" : 3, "diagnosisCodeableConcept" : { "coding": [{"code": "{{msg.DX_3}}"}]}}],'
-
 trans_claim_procedure = '"procedure": [{"sequence": 1,"type": [{"coding": [{"code": "primary"}]}],"date": "{{msg.ServiceFromDate /Date_of_Service | date: "yyyy-MM-dd"}}","procedureCodeableConcept": {"coding": [{"code": "{{msg.ProcedureCode /CPTCode_Primary}}"}]}},{"sequence": 2,"type": [{"coding": [{"code": "secondary"}]}],"date": "{{msg.ServiceFromDate /Date_of_Service | date: "yyyy-MM-dd"}}","procedureCodeableConcept": {"coding": [{"code": "{{msg.ProcedureCode2}}"}]}}],  '
-
 trans_claims_insure =   '"insurance" : [{ /"sequence" : 1, "focal" : true, "coverage" : {"reference" : "Coverage/{{msg.CoverageID}}"} }],'
-
 trans_claims_total = '"total" :{"value": "{{msg.BillAmount}}" }'
 
 class TestliqTransform (unittest.TestCase):
